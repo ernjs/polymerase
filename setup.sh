bcd105d72754e90605d1fe9a8a077991a197eccb
Ernando Souza <ernjs@outlook.com>
2017-12-04 22:35:19 -0200

    Setup

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# @todo Create a makefile for make install
+
+# @todo Setup for multiple shells 
+PROFILE=~/.zshrc;
+PATH=$(pwd)
+
+echo "PATH_POLYMERASE=$PATH" >> $PROFILE;
+
+polymerase="docker-compose -f $PATH/docker-compose.yml run --rm"
+
+echo "alias polymerase='$polymerase'" >> $PROFILE
+echo "alias polymerase-deploy='$polymerase -u $UID deploy'" >> $PROFILE
+echo "alias polymerase-web='$polymerase web'" >> $PROFILE
