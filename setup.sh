56bafaa86fa98f7833defdfed69444a2a5453caa
Ernando Souza <ernjs@outlook.com>
2017-12-06 23:01:09 -0200

    Added function (bash) to facilitate usage

--- setup.sh
+++ setup.sh
@@ -4,13 +4,32 @@
 
 # @todo Setup for multiple shells 
 PROFILE=~/.zshrc;
-PATH=$(pwd)
 
-echo "PATH_POLYMERASE=$PATH" >> $PROFILE;
+cat << EOF >> $PROFILE
+function polymerase () {
+	remove=''
+	args_jump=2
 
-polymerase="docker-compose -f $PATH/docker-compose.yml run --name " 
-polymerase+='${PWD##*/}'
+	if [[ "PRS_FIRST_ARG" = 'rm' ]]; then
+		remove+='--rm'
+		args_jump=3
+	fi
 
-echo "alias polymerase='$polymerase'" >> $PROFILE
-echo "alias polymerase-deploy='$polymerase -u $UID deploy'" >> $PROFILE
-echo "alias polymerase-web='$polymerase web'" >> $PROFILE
+	if [[ "PRS_FIRST_ARG" = 'deploy' ]] || [[ "PRS_SECOND_ARG" = 'deploy' ]]; then
+		PRS_COMMAND PRS_REMOVE -u PRS_UID deploy PRS_ARGS
+		return
+	fi
+
+	if [[ "PRS_FIRST_ARG" = 'web' ]] || [[ "PRS_SECOND_ARG" = 'web' ]]; then
+		PRS_COMMAND PRS_REMOVE web PRS_ARGS
+		return
+	fi
+}
+EOF
+
+sed -i 's/PRS_FIRST_ARG/$1/g' $PROFILE
+sed -i 's/PRS_SECOND_ARG/$2/g' $PROFILE
+sed -i "s#PRS_COMMAND#docker-compose -f $(pwd)\/docker-compose.yml run --name ${PWD##*/}#g" $PROFILE
+sed -i 's/PRS_REMOVE/$remove/g' $PROFILE
+sed -i 's/PRS_UID/$UID/g' $PROFILE
+sed -i 's/PRS_ARGS/${@:$args_jump}/g' $PROFILE
