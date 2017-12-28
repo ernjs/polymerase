659cef739091d0add4d4d0820558d3ba178828aa
Ernando Souza <ernjs@outlook.com>
2017-12-27 23:29:48 -0200

    Simplify setup of polymerase command

--- setup.sh
+++ setup.sh
@@ -8,8 +8,6 @@ INSTALL_DIR=/usr/bin/polymerase
 
 cat << EOF > $INSTALL_DIR
 #!/bin/bash
-
-function polymerase () {
 	remove=''
 	args_jump=2
 
@@ -20,21 +18,15 @@ function polymerase () {
 
 	if [[ "PRS_FIRST_ARG" = 'deploy' ]] || [[ "PRS_SECOND_ARG" = 'deploy' ]]; then
 		PRS_COMMAND PRS_REMOVE -u PRS_UID deploy PRS_ARGS
-		return
-	fi
-
-	if [[ "PRS_FIRST_ARG" = 'web' ]] || [[ "PRS_SECOND_ARG" = 'web' ]]; then
+	elif [[ "PRS_FIRST_ARG" = 'web' ]] || [[ "PRS_SECOND_ARG" = 'web' ]]; then
 		PRS_COMMAND PRS_REMOVE web PRS_ARGS
-		return
 	fi
-}
 EOF
 
 sed -i 's/PRS_FIRST_ARG/$1/g' $INSTALL_DIR
 sed -i 's/PRS_SECOND_ARG/$2/g' $INSTALL_DIR
 sed -i "s#PRS_COMMAND#docker-compose -f $(pwd)\/docker-compose.yml run --name PRS_PWD#g" $INSTALL_DIR
 sed -i 's/PRS_REMOVE/$remove/g' $INSTALL_DIR
-sed -i 's/PRS_UID/$UID/g' $INSTALL_DIR
 sed -i 's/PRS_ARGS/${@:$args_jump}/g' $INSTALL_DIR
 sed -i 's%PRS_PWD%${PWD##*/}%g' $INSTALL_DIR
 
